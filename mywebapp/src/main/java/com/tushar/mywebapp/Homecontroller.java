8b86058b38e51538c11b2698a8d3aa9aacfaf8ab
Tushar-cyber16 <ctr79302@gmail.com>
2024-05-25 19:31:54 +0530

    get data from browser

--- mywebapp/src/main/java/com/tushar/mywebapp/Homecontroller.java
+++ mywebapp/src/main/java/com/tushar/mywebapp/Homecontroller.java
@@ -2,17 +2,24 @@ package com.tushar.mywebapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class Homecontroller {
 
     @RequestMapping("home")
     // @ResponseBody
-    public String home()
+    public String home(HttpServletRequest req )
     {
+        HttpSession session = req.getSession();
+        String name = req.getParameter("name");
+
+        session.setAttribute("name", name);
         System.out.println("home controller");
-        return "home.jsp";
+        return "home";
     }
     
 }
