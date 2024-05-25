9fa2c36128a7e0ae6753b9e259fe835c16424c77
Tushar-cyber16 <ctr79302@gmail.com>
2024-05-25 19:38:46 +0530

    with different keys for same value at browser and at controller

--- mywebapp/src/main/java/com/tushar/mywebapp/Homecontroller.java
+++ mywebapp/src/main/java/com/tushar/mywebapp/Homecontroller.java
@@ -3,6 +3,7 @@ package com.tushar.mywebapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -12,9 +13,9 @@ public class Homecontroller {
 
     @RequestMapping("home")
     // @ResponseBody
-    public String home(String name , HttpSession session )
+    public String home(@RequestParam("name") String myname , HttpSession session )
     {
-        session.setAttribute("name", name);
+        session.setAttribute("name", myname);
         System.out.println("home controller");
         return "home";
     }
